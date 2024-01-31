ee0b92930b00eadc3c8e280aecd605a8a5185b05
Alex Grimit <u1310325@utah.edu>
2024-01-30 19:09:06 -0700

    lab3

---
+++ lab3/basic_python_grimit_alex.py
@@ -0,0 +1,22 @@
+#This is a single line comment
+print("Chewbacca")
+print(3.4)
+user_name = "Chewbacca"
+print(user_name)
+number=20
+number+=20
+print(number)
+print("my name is " + user_name)
+num=12345*67890
+print(str(num)[1:3])
+def add_numbers(a,b):
+    output=a+b
+    return output
+print(add_numbers(3,4))
+name="Alex"
+def say_hello():
+    name="Kate"
+    return f"hello {name}"
+
+print(say_hello())
+
---
+++ lab3/python_expressions_control_grimit_alex.py
@@ -0,0 +1,27 @@
+x=-2
+y=3*(abs(2*x)+4)
+print(f"a: {20>9}")
+print(2-3*4)
+print(f"is 'matt' == 'matthew'? {'matt'=='matthew'}")
+print(f"is 7 <= 8? {7<=8}")
+bank_balance = 50
+if bank_balance > 100:
+    money = 1000
+    bank_balance += money
+else:
+    print("balance is less than or equal to 100.")
+fuel = 1
+print("Fill tank now" if fuel<=1 else "There's enough fuel")
+books = ["harry potter", "lord of the rigs", "matts autobiography"]
+for book in books:
+    print(f"book: {book}")
+
+for i in range(3):
+    print(f"i: {i}")
+    if i == 2:
+        break
+
+
+
+
+         
